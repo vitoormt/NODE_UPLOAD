a75b83226c66116e6ea44a67078137d606afd9a8
vitoormt <84139448+vitoormt@users.noreply.github.com>
2022-10-25 11:26:29 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,15 +5,32 @@ app.use(express.json())
 app.set('view engine','ejs')
 
 
+//multer
+const multer = require('multer')
+//storage
+const storage = multer.diskStorage({
+    //destino
+    destination: (req, res, cb) => {
+        cb(null,'uploads')
+    },
+    //nome do arquivo
+    filename: (req, file, cb) => {
+        cb(null,file.originalname)
+    },
+})
+
+const upload = multer ({storage})
+
 //rota padrão
 app.get('/', (req,res)=>{
     res.render('index.ejs')
 })
 
 //upload
-app.post('/', (req,res) => {
+//midleware
+app.post('/',upload.single('arquivo'), (req,res) => {
     res.send("OK")
-    console.log(res.body,req.file) //apoio a lógica
+    console.log(req.body,req.file) //apoio a lógica
 })
 
 //escuta do servidor
